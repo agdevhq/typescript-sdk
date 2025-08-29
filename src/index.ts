84f41b5427d4fcb1eb4630432f67434725774ea9
Florian Reifschneider <florian@ag.dev>
2025-08-28 23:42:31 -0700

    feat: create index file to export agent and client modules

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from './agent.ts';
+export * from './client.ts';
+export type {
+    Agent,
+    AgentRun,
+    AgentRunEvent,
+    AgentTool,
+    CreateAgentRequest,
+    UpdateAgentRequest,
+} from './types.ts';
